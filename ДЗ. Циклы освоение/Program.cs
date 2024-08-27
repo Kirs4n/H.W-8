a86a0cdfa9fa79007ff2147ef7e42036c75d9606
KirKiller <KirKiller@DESKTOP-1JIA97R>
2024-08-27 22:28:02 +0500

    Добавьте файлы проекта.

---
+++ ДЗ. Циклы освоение/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Runtime.InteropServices;
+
+namespace ДЗ.Циклы_освоение
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int beforLaunch;
+            Console.Write(" Установите отщет до запуска ракеты: ");
+            beforLaunch = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Запуск произойдет через: ");
+
+            while (beforLaunch-->0)
+            {
+                Console.WriteLine(beforLaunch);
+            }
+        }
+    }
+}
